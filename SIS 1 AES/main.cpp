ea9d32fe0a8e1fe487911f2b700df6df58fa0d9b
ZarinaKab <another.zarina@gmail.com>
2024-02-05 19:48:05 +0600

    pre-final dec and key gen

--- SIS 1 AES/main.cpp
+++ SIS 1 AES/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include <random>
+#include <string>
 #include "AES.cpp"
 
 using namespace std;
@@ -11,31 +12,22 @@ void phex(unsigned char x){
 }
 
 int main(int argc, char* argv[]){
-    {
-        string sdata = "Blowfish";
-        vector<unsigned char> data(sdata.begin(), sdata.end());
-        string skey = "1a3d2sa1d3a4sd64";
-        array<unsigned char, 16> key;
-        for(int i = 0; i < 16; i++) key[i] = skey[i];
+    vector<unsigned char> res;
+    array<unsigned char, 16> key;
+    string wtf;
 
-        auto res = AES::AES128(key).encrypt(data);
-        // auto res_dec = AES::AES128(key).decrypt(data);
-        // cout << "Data:   "; for(auto x : data) phex(x); cout << '\n';
-        // cout << "Key:    "; for(auto x : key) phex(x); cout << '\n';
-        // cout << "Result: "; for(auto x : res) phex(x); cout << '\n';
-    }
-    if (argc != 4) {
-        cerr << "Usage: " << argv[0] << " <input_file> <output_file> <encryption_key>" << endl;
+    cout << "What do you wanna do with data? (enc or dec): " << wtf << " ";
+    cin >> wtf;
+
+ 
+    // Check filename
+    if (argc != 3) {
+        cerr << "Usage: " << argv[0] << " <input_file> <output_file>" << endl;
         return 1;
     }
 
-    string skey = argv[3];
-    array<unsigned char, 16> key;
-    for(int i = 0; i < 16 && i < skey.size(); i++) {
-        key[i] = skey[i];
-    }
 
-    // Чтение данных из файла
+    // Reading a data from file
     ifstream inputFile(argv[1], ios::binary);
     if (!inputFile) {
         cerr << "Error: Unable to open input file." << endl;
@@ -45,11 +37,52 @@ int main(int argc, char* argv[]){
     vector<unsigned char> data((istreambuf_iterator<char>(inputFile)), istreambuf_iterator<char>());
     inputFile.close();
 
-    // Шифрование данных
-    auto res = AES::AES128(key).decrypt(data);
-    // auto res = AES::AES128(key).encrypt(data);
+    if(wtf == "enc") {
+        
+        // Creating key generator
+        random_device rd;  // a seed source for the random number engine
+        mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+        uniform_int_distribution<> distrib(0, 255);
+        
+        for(int i = 0; i < 16; i++) {
+            key[i] = distrib(gen);
+        }
 
-    // Запись зашифрованных данных в файл
+        ofstream outputFile("key.txt");
+        if (!outputFile) {
+            cerr << "Error: Unable to open output file." << endl;
+            return 1;
+        }
+        outputFile.write(reinterpret_cast<const char*>(key.data()), key.size());
+        size_t data_size = data.size();
+        cout << "DATA SIZE: " << data_size << '\n';
+        outputFile.write(reinterpret_cast<const char*>(&data_size), sizeof(data_size));
+        outputFile.close();
+
+        // Encryption
+        res = AES::AES128(key).encrypt(data);
+        cout << "Encryption completed successfully." << endl;
+    } else {
+        ifstream inputFile("key.txt", ios::binary);
+        // Checking to correctly opens
+        if (!inputFile) {
+            cerr << "Error: Unable to open the file." << endl;
+            return 1;
+        }
+        
+        inputFile.read(reinterpret_cast<char*>(key.data()), key.size());
+        size_t data_size = 0;
+        inputFile.read(reinterpret_cast<char*>(&data_size), sizeof(data_size));
+        cout << "DATA SIZE: " << data_size << '\n';
+        inputFile.close();
+
+        //Decryption
+        res = AES::AES128(key).decrypt(data);
+        res.resize(data_size);
+        cout << "Decryption completed successfully." << endl;
+    }
+
+    // Record the data
     ofstream outputFile(argv[2], ios::binary);
     if (!outputFile) {
         cerr << "Error: Unable to open output file." << endl;
@@ -59,11 +92,9 @@ int main(int argc, char* argv[]){
     outputFile.write(reinterpret_cast<const char*>(res.data()), res.size());
     outputFile.close();
 
-    cout << "Encryption completed successfully." << endl;
-
-    cout << "Data:   "; for(auto x : data) phex(x); cout << '\n';
+    // cout << "Data:   "; for(auto x : data) phex(x); cout << '\n';
     cout << "Key:    "; for(auto x : key) phex(x); cout << '\n';
-    cout << "Result: "; for(auto x : res) phex(x); cout << '\n';
+    // cout << "Result: "; for(auto x : res) phex(x); cout << '\n';
 
     return 0;
 }
